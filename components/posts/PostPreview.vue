888173db9759095d9c33271b38d839c11c9fba30
Le Dinh Anh Tan <tanle@desktop-linux-tanle.localdomain>
2022-05-09 13:39:51 +0700

    feat: navigation

---
+++ components/posts/PostPreview.vue
@@ -0,0 +1,69 @@
+<template>
+  <nuxt-link :to="'/posts/' + post.id" class="post-preview">
+    <article>
+      <div
+        class="post-thumbnail"
+        :style="{ backgroundImage: 'url(' + post.image + ')' }"
+      ></div>
+      <div class="post-content">
+        <h1>{{ post.title }}</h1>
+        <p>{{ post.preview }}</p>
+      </div>
+    </article>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  name: 'PostPreview',
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style coped>
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: white;
+  width: 90%;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+    margin: 10px;
+  }
+}
+
+.post-thumbnail {
+  z-index: 1;
+  width: 100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+  position: relative;
+}
+
+.post-content {
+  z-index: 2;
+  padding: 10px;
+  text-align: center;
+  position: relative;
+  background-color: white;
+}
+
+a:hover .post-content,
+a:active .post-content {
+  background-color: #ccc;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,31 +4,16 @@
       <h1>Get the latest tech news!</h1>
     </section>
     <section class="featured-posts">
-      <nuxt-link
-        :to="toPost"
-        class="post-preview"
-        v-for="post in posts"
-        :key="post.id"
-      >
-        <article>
-          <div
-            class="post-thumbnail"
-            :style="{ backgroundImage: 'url(' + post.image + ')' }"
-          ></div>
-          <div class="post-content">
-            <h1>{{ post.title }}</h1>
-            <p>{{ post.preview }}</p>
-          </div>
-        </article>
-      </nuxt-link>
+      <PostPreview v-for="post in posts" :key="post.id" :post="post" />
     </section>
   </div>
 </template>
 
 <script>
+import PostPreview from '@/components/posts/PostPreview.vue'
 export default {
   name: 'IndexPage',
-
+  components: { PostPreview },
   data() {
     return {
       posts: [
@@ -49,7 +34,6 @@ export default {
       ],
     }
   },
-
   computed: {
     toPost() {
       return `/posts/2`
@@ -66,6 +50,7 @@ export default {
   box-sizing: border-box;
   background-position: center;
   background-size: cover;
+  background-image: url('~assets/images/main-page-background.jpg');
 }
 
 .intro h1 {
@@ -96,46 +81,5 @@ export default {
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
-}
-
-.post-preview {
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 2px #ccc;
-  background-color: white;
-  width: 90%;
-}
-
-a {
-  text-decoration: none;
-  color: black;
-}
-
-@media (min-width: 850px) {
-  .post-preview {
-    width: 400px;
-    margin: 10px;
-  }
-}
-
-.post-thumbnail {
-  z-index: 1;
-  width: 100%;
-  height: 200px;
-  background-position: center;
-  background-size: cover;
-  position: relative;
-}
-
-.post-content {
-  z-index: 2;
-  padding: 10px;
-  text-align: center;
-  position: relative;
-  background-color: white;
-}
-
-a:hover .post-content,
-a:active .post-content {
-  background-color: #ccc;
 }
 </style>
